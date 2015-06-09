7bd123ce450969e41f679f281d4ff5a629c685b5
gabrielblanco <gabriel.blanco03@hotmail.com>
2015-06-08 20:10:13 -0600

    Login - Metodos
    Se cambió la interfaz del ligin y se crearon las clases de los métodos
    (producto-usuario-transaccion)

---
+++ Proyect1/Models/Products.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyect1.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Status { get; set; }
+        public DateTime RegisterDate { get; set; }
+    }
+}
---
+++ Proyect1/Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyect1.Models
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+        public string FeaturedProduct { get; set; }
+        public string ProductOfInteres { get; set; }
+        public DateTime Date { get; set; }
+        public string Status { get; set; }
+        public string Detail { get; set; }
+    }
+}
